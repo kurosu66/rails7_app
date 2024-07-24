daa20ead22a4e3efa00bdd3e3b764197be4c428f
kurosu <kurosu.66@gmail.com>
2024-07-25 07:47:05 +0900

    5.3.3 - 5.3.4リンクのテストまで

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class ApplicationHelperTest < ActionView::TestCase
+  test "full title helper" do
+    assert_equal 'Ruby on Rails Tutorial Sample App' , full_title
+    assert_equal 'Help | Ruby on Rails Tutorial Sample App', full_title("Help")
+  end
+end
