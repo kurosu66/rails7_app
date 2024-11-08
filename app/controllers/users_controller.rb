a2e69c0ae068405a58085fd0d7388568eb7c3ea8
kitarou888 <85793702+kitarou888@users.noreply.github.com>
2024-11-08 21:33:03 +0900

    7.3.3まで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,19 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 保存の成功をここで扱う
+    else
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
