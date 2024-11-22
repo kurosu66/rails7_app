08fe0885491dcdb1743ac03f52441114fb99bfe2
kurosu <kurosu.66@gmail.com>
2024-11-22 21:05:54 +0900

    7.4.4成功時のテスト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # 保存の成功をここで扱う
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render "new", status: :unprocessable_entity
     end
