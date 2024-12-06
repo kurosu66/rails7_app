67a652af06b952f8ed3407b878ae8745c1b59cdb
kurosu <kurosu.66@gmail.com>
2024-12-07 08:08:03 +0900

    8.2 ログイン

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
 
   def hello
     render html: "hello, world!"
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      reset_session      # ログインの直前に必ずこれを書くこと
+      log_in user
+      redirect_to user
+    else
+      flash[:danger] = 'Invalid email/password combination' # 本当は正しくない
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
+end
