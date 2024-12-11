188bf8e57159643ff5e995a77ffcd7acc91c9fc7
kurosu <kurosu.66@gmail.com>
2024-12-11 22:52:10 +0900

    8.2.2

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,14 @@
 module SessionsHelper
 
-    # 渡されたユーザーでログインする
-    def log_in(user)
-      session[:user_id] = user.id
+  # 渡されたユーザーでログインする
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # 現在ログイン中のユーザーを返す（いる場合）
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
     end
+  end
 end
