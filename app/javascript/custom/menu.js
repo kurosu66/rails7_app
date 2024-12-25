1093e177ab8659ca68b87494e99a9f626e8ad1fe
kurosu <kurosu.66@gmail.com>
2024-12-25 21:01:01 +0900

    8.2.6 レイアウトの変更をテストする 演習の前

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,13 +1,17 @@
 // メニュー操作
 
-// トグルリスナーを追加してクリックをリッスンする
-document.addEventListener("turbo:load", function() {
-    let account = document.querySelector("#account");
-    if (account) {
-      account.addEventListener("click", function(event) {
-        event.preventDefault();
-        let menu = document.querySelector("#dropdown-menu");
-        menu.classList.toggle("active");
-      });
-    }
+// トグルリスナーを追加する
+function addToggleListener(selected_id, menu_id, toggle_class) {
+  let selected_element = document.querySelector(`#${selected_id}`);
+  selected_element.addEventListener("click", function(event) {
+    event.preventDefault();
+    let menu = document.querySelector(`#${menu_id}`);
+    menu.classList.toggle(toggle_class);
   });
+}
+
+// クリックをリッスンするトグルリスナーを追加する
+document.addEventListener("turbo:load", function() {
+  addToggleListener("hamburger", "navbar-menu",   "collapse");
+  addToggleListener("account",   "dropdown-menu", "active");
+});
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,6 +2,20 @@ require "test_helper"
 require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
+  test "login with valid information" do
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
 
   test "login with invalid information" do
     get login_path
@@ -11,6 +25,6 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
-    assert flash.empty?
+    assert flash.empty? #ここでテスト落ちる 2024/12/25
   end
 end
