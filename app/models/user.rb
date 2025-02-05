fe8045f0117fe202b1db2c03b6bbcb7c8a8304c8
kurosu <kurosu.66@gmail.com>
2025-02-05 20:05:41 +0900

    9.3まで

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -14,5 +15,15 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # 永続的セッションのためにユーザーをデータベースに記憶する
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
---
+++ db/migrate/20250131125054_add_remember_digest_to_users.rb
@@ -0,0 +1,5 @@
+class AddRememberDigestToUsers < ActiveRecord::Migration[7.2]
+  def change
+    add_column :users, :remember_digest, :string
+  end
+end
