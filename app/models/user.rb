667908e4e0c64410cf94a748c43e0bb72414114d
kurosu <kurosu.66@gmail.com>
2025-02-14 21:30:58 +0900

    9.6 authenticated?メソッド

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,4 +29,9 @@ class User < ApplicationRecord
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
+
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
