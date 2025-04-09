dff8fc0a747c8c4410ab9ec9b09b54ee764b2890
kitarou888 <yuichiro.makita24@gmail.com>
2025-04-09 21:17:58 +0900

    9.3.1 演習の手前まで

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -78,3 +78,18 @@ class LogoutTest < Logout
     assert_redirected_to root_url
   end
 end
+
+class RememberingTest < UsersLogin
+  test "login with remembering" do
+    log_in_as(@user, remember_me: "1")
+    assert_not cookies[:remember_token].blank?
+  end
+
+  test "login without remembering" do
+    # Cookieを保存してログイン
+    log_in_as(@user, remember_me: "1")
+    # Cookieが削除されていることを検証してからログイン
+    log_in_as(@user, remember_me: "0")
+    assert cookies[:remember_token].blank?
+  end
+end
