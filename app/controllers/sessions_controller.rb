9f770031d08e70a9cded63ac5bee1920976adcfc
kurosu <kurosu.66@gmail.com>
2025-04-09 20:13:06 +0900

    4/2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
       reset_session      # セッション固定攻撃対策用
-      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       log_in user
       redirect_to user
     else
