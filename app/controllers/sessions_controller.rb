8b6b3da35e4d7f157500d09dd5583ecacc7d8f0e
kitarou888 <yuichiro.makita24@gmail.com>
2025-04-11 22:28:06 +0900

    9.3.1 おわりまで

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user&.authenticate(params[:session][:password])
       reset_session      # セッション固定攻撃対策用
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      log_in user
-      redirect_to user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      log_in @user
+      redirect_to @user
     else
       flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない
       render 'new', status: :unprocessable_entity
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -82,7 +82,7 @@ end
 class RememberingTest < UsersLogin
   test "login with remembering" do
     log_in_as(@user, remember_me: "1")
-    assert_not cookies[:remember_token].blank?
+    assert_equal cookies[:remember_token], assigns(:user).remember_token
   end
 
   test "login without remembering" do
