20f61f4fe13aab201bb39c6f89909bde9c360822
kitarou888 <yuichiro.makita24@gmail.com>
2025-05-14 21:17:58 +0900

    10.2.1 おわりまで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -41,5 +42,13 @@ class UsersController < ApplicationController
       params
         .require(:user)
         .permit(:name, :email, :password, :password_confirmation)
+    end
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url, status: :see_other
+      end
     end
 end
