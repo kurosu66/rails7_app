6a44bdd771c3837fbd39e3a5cccf5456958d6d85
kitarou888 <yuichiro.makita24@gmail.com>
2025-07-09 21:00:56 +0900

    10.2.2 演習終了

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -34,5 +34,6 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal name,  @user.name
     assert_equal email, @user.email
+    assert_equal nil, session[:forwarding_url]
   end
 end
