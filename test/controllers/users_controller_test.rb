d228d2259da2209c1e5a1f49cec172bbc078cce3
kurosu <kurosu.66@gmail.com>
2025-07-16 21:06:10 +0900

    10.3 すべてのユーザーを表示する

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -43,4 +43,9 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert flash.empty?
     assert_redirected_to root_url
   end
+
+    test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,17 +1,25 @@
 require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
 
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", users_path, false
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
+
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", users_path
   end
 end
