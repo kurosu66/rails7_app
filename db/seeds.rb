3d0f628c1f012b78b267d6ca2f1f7ead5ff85f86
kitarou888 <yuichiro.makita24@gmail.com>
2025-08-06 20:54:51 +0900

    10.4.1 終了

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,8 @@ User.create!(
   name: "Example User",
   email: "example@railstutorial.org",
   password: "foobar",
-  password_confirmation: "foobar"
+  password_confirmation: "foobar",
+  admin: true
 )
 
 # 追加のユーザーをまとめて生成する
