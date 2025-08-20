5ef47e254d14b490a0f62405aab276ad0b6f8e75
kurosu <kurosu.66@gmail.com>
2025-08-20 20:44:15 +0900

    11.2の内容

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,9 @@ User.create!(
   email: "example@railstutorial.org",
   password: "foobar",
   password_confirmation: "foobar",
-  admin: true
+  admin: true,
+  activated: true,
+  activated_at: Time.zone.now
 )
 
 # 追加のユーザーをまとめて生成する
@@ -24,6 +26,8 @@ User.create!(
     name: name,
     email: email,
     password: password,
-    password_confirmation: password
+    password_confirmation: password,
+    activated: true,
+    activated_at: Time.zone.now
   )
 end
