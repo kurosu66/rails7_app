c8a59a34a9848100d34ac29ea6575cf5ffba6590
kurosu <kurosu.66@gmail.com>
2025-08-29 20:34:48 +0900

    12.2.2送信メールのテスト

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  host = 'localhost:3009'
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 end
